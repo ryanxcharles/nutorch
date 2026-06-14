d84f0bbeef1a2649435b5facb92b134b4873c265
Ryan X. Charles <ryan@ryanxcharles.com>
2026-06-14 16:50:38 -0500

    Update formula for 1.0.0 source

--- dist/nutorch.rb
+++ dist/nutorch.rb
@@ -7,7 +7,7 @@ class Nutorch < Formula
   desc "GPU tensor daemon and CLI for any shell (Apple-silicon MPS, PyTorch-powered)"
   homepage "https://github.com/nutorch/nutorch"
   url "https://github.com/nutorch/nutorch/releases/download/v1.0.0/nutorch-1.0.0.tar.gz"
-  sha256 "a50d68d4ec770b0b742cf07e89c9947597b7989d6be9597c8f183a17bbfd6c21"
+  sha256 "3619bc009757ba89ac79f142ba14583281959d494aa366cc08a094905855483f"
   license "MIT"
 
   depends_on "rust" => :build
